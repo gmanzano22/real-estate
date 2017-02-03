23a77ef55b33b7c51589e4aa0db7f9bf09fae372
Guillaume <guillaume.fj.manzano@gmail.com>
2017-02-03 13:13:46 +0100

    start tp3

--- app.js
+++ app.js
@@ -12,14 +12,12 @@ app.set( 'view engine', 'ejs' );
 //setting the 'assets' directory as our static assets dir (css, js, img, etc...)
 app.use( '/assets', express.static( 'assets' ) );
 
-
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
     res.render( 'home', {
         message: 'The Home Page!'
     });
 });
-
 
 //launch the server on the 3000 port
 app.listen( 3000, function () {
