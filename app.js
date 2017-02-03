fb4ceff5682036afc4a7091bf2b0c7b3c21f84e2
Guillaume <guillaume.fj.manzano@gmail.com>
2017-02-03 15:56:02 +0100

    stzrt tp3

--- app.js
+++ app.js
@@ -6,6 +6,35 @@ var cheerio = require( 'cheerio' );
 //creating a new express server
 var app = express();
 
+
+function callLeboncoin() {
+    var url = 'https://www.leboncoin.fr/ventes_immobilieres/1087631966.htm?ca=12_s'
+
+
+
+    request( url, function ( error, response, html ) {
+        if ( !error && response.statusCode == 200 ) {
+            const $ = cheerio.load( html )
+            const lbcDataArray = $( 'section.properties span.value' )
+
+            let lbcData = {
+                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /_!\s/g, '-' ),
+                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
+                surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
+
+            }
+            console.log( "data", lbcData );
+        }
+
+
+        else {
+            console.log( "error", error )
+        }
+    })
+
+}
+
 //setting EJS as the templating engine
 app.set( 'view engine', 'ejs' );
 
@@ -14,9 +43,14 @@ app.use( '/assets', express.static( 'assets' ) );
 
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
+
+    callLeboncoin();
+
     res.render( 'home', {
-        message: 'The Home Page!'
+        message: 'OK'
+
     });
+
 });
 
 //launch the server on the 3000 port
